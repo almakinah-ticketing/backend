b28a4b7f53dac76ededd55d397ce08e12cd0ea0b
ahmadhzayed <ahmad_hassan_zayed@hotmail.com>
2017-12-25 18:15:25 +0200

    modifing routes

--- almakinah_summit/config/routes.rb
+++ almakinah_summit/config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   end
   resources :attendees
   resources :admins
+  resources :events
+  resources :types
+  resources :tickets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
